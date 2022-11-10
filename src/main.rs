424074b291bb0ad23795ef0469edacadab9af333
Riddle <samuel.riddle1@gmail.com>
2022-11-09 21:53:02 -0700

    Simple output to setup output stream to

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
+use std::collections::hash_map::RandomState;
 use std::str::FromStr;
 
 use std::collections::{HashMap, HashSet};
 
 use clap::Parser;
+use near_lake_framework::near_indexer_primitives::views::BlockView;
+use serde_json::value::Index;
 use tokio::sync::mpsc;
 use tracing::info;
 
-use near_lake_framework::near_indexer_primitives;
+use near_lake_framework::near_indexer_primitives::{self, IndexerShard};
 use near_lake_framework::LakeConfig;
 
 use configs::{init_logging, Opts};
 
 mod configs;
 
-/// Assuming we want to watch for transactions where a receiver account id is one of the provided in a list
-/// We pass the list of account ids (or contracts it is the same) via argument ``--accounts``
-/// We want to catch all *successfull* transactions sent to one of the accounts from the list.
-/// In the demo we'll just look for them and log them but it might and probably should be extended based on your needs.
+pub struct StreamResult {
+    pub block: BlockView,
+    pub index_shards: Vec<IndexerShard>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), tokio::io::Error> {
@@ -28,15 +31,7 @@ async fn main() -> Result<(), tokio::io::Error> {
 
     let (_, stream) = near_lake_framework::streamer(config);
 
-    let watching_list = opts
-        .accounts
-        .split(',')
-        .map(|elem| {
-            near_indexer_primitives::types::AccountId::from_str(elem).expect("AccountId is invalid")
-        })
-        .collect();
-
-    listen_blocks(stream, watching_list).await;
+    listen_blocks(stream).await;
 
     Ok(())
 }
@@ -45,98 +40,24 @@ async fn main() -> Result<(), tokio::io::Error> {
 /// and perform necessary checks
 async fn listen_blocks(
     mut stream: mpsc::Receiver<near_indexer_primitives::StreamerMessage>,
-    watching_list: Vec<near_indexer_primitives::types::AccountId>,
 ) {
     eprintln!("listen_blocks");
     // This will be a map of correspondence between transactions and receipts
-    let mut tx_receipt_ids = HashMap::<String, String>::new();
+    // let mut stream_result  = StreamResult::new();
     // This will be a list of receipt ids we're following
-    let mut wanted_receipt_ids = HashSet::<String>::new();
+    let mut block_shards: Vec<IndexerShard> = Vec::new();
+    // let mut output: HashSet<StreamResult> = HashSet::new();
+
 
     // Boilerplate code to listen the stream
     while let Some(streamer_message) = stream.recv().await {
-        eprintln!("Block height: {}", streamer_message.block.header.height);
+        eprintln!("Streamer Message: {:#?}", streamer_message);
+        
         for shard in streamer_message.shards {
-            let chunk = if let Some(chunk) = shard.chunk {
-                chunk
-            } else {
-                continue;
-            };
-
-            for transaction in chunk.transactions {
-                // Check if transaction receiver id is one of the list we are interested in
-                if is_tx_receiver_watched(&transaction, &watching_list) {
-                    // extract receipt_id transaction was converted into
-                    let converted_into_receipt_id = transaction
-                        .outcome
-                        .execution_outcome
-                        .outcome
-                        .receipt_ids
-                        .first()
-                        .expect("`receipt_ids` must contain one Receipt Id")
-                        .to_string();
-                    // add `converted_into_receipt_id` to the list of receipt ids we are interested in
-                    wanted_receipt_ids.insert(converted_into_receipt_id.clone());
-                    // add key value pair of transaction hash and in which receipt id it was converted for further lookup
-                    tx_receipt_ids.insert(
-                        converted_into_receipt_id,
-                        transaction.transaction.hash.to_string(),
-                    );
-                }
-                
-            }
-
-            for execution_outcome in shard.receipt_execution_outcomes {
-                if let Some(receipt_id) =
-                    wanted_receipt_ids.take(&execution_outcome.receipt.receipt_id.to_string())
-                {
-                    // log the tx because we've found it
-                    info!(
-                        target: "indexer_example",
-                        "Transaction hash {:?} related to {} executed with status {:?}",
-                        tx_receipt_ids.get(receipt_id.as_str()),
-                        &execution_outcome.receipt.receiver_id,
-                        execution_outcome.execution_outcome.outcome.status
-                    );
-                    if let near_indexer_primitives::views::ReceiptEnumView::Action {
-                        signer_id,
-                        ..
-                    } = &execution_outcome.receipt.receipt
-                    {
-                        eprintln!("{}", signer_id);
-                    }
-
-                    if let near_indexer_primitives::views::ReceiptEnumView::Action {
-                        actions, ..
-                    } = execution_outcome.receipt.receipt
-                    {
-                        for action in actions.iter() {
-                            if let near_indexer_primitives::views::ActionView::FunctionCall {
-                                args,
-                                ..
-                            } = action
-                            {
-                                if let Ok(decoded_args) = base64::decode(args) {
-                                    if let Ok(args_json) =
-                                        serde_json::from_slice::<serde_json::Value>(&decoded_args)
-                                    {
-                                        eprintln!("{:#?}", args_json);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    // remove tx from hashmap
-                    tx_receipt_ids.remove(receipt_id.as_str());
-                }
-            }
+            block_shards.push(shard);
         }
-    }
-}
 
-fn is_tx_receiver_watched(
-    tx: &near_indexer_primitives::IndexerTransactionWithOutcome,
-    watching_list: &[near_indexer_primitives::types::AccountId],
-) -> bool {
-    watching_list.contains(&tx.transaction.receiver_id)
+        eprintln!(" Block: {:#?}", streamer_message.block);
+        eprintln!(" Shards: {:#?}", block_shards);
+    }
 }
